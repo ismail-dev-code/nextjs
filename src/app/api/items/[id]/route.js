2459ca4ecaf38c1f65967def81472b44430fe037
ismail-dev-code <hm.ismail772@gmail.com>
2025-07-29 12:24:15 +0600

    feat: api added

---
+++ src/app/api/items/[id]/route.js
@@ -0,0 +1,36 @@
+import dbConnect, { collectionNames } from "@/lib/dbConnect";
+import { ObjectId } from "mongodb";
+
+export async function GET(req, { params }) {
+  const p = await params;
+  console.log(p);
+  const singleData = await dbConnect(collectionNames.products).findOne({
+    _id: new ObjectId(p.id),
+  });
+
+  return Response.json(singleData);
+}
+
+export async function DELETE(req, { params }) {
+  const p = await params;
+  console.log(p);
+  const response = await dbConnect(collectionNames.products).deleteOne({
+    _id: new ObjectId(p.id),
+  });
+
+  return Response.json(response);
+}
+
+export async function PATCH(req, { params }) {
+  const p = await params;
+  console.log(p);
+  const postedData = await req.json();
+  const filter = { _id: new ObjectId(p.id) };
+  const updatedResponse = await dbConnect(collectionNames.products).updateOne(
+    filter,
+    { $set: { ...postedData } },
+    { upsert: true }
+  );
+
+  return Response.json(updatedResponse);
+}
---
+++ src/app/api/items/route.js
@@ -0,0 +1,18 @@
+import dbConnect, { collectionNames } from "@/lib/dbConnect"
+import { revalidatePath } from "next/cache"
+
+export async function GET() {
+
+    const data = await dbConnect().find({}).toArray()
+
+    return Response.json(data)
+}
+
+export async function POST(req) {
+
+
+    const postedData = await req.json()
+    const result = await dbConnect(collectionNames.products).insertOne(postedData)
+    revalidatePath("/products")
+    return Response.json(result)
+}
